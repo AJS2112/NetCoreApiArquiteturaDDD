521b07b381cbb241164dc355dcb2d1a3f0e62e5b
Antonio Sucre <antonio.sucre@indikatore.com>
2021-12-23 11:16:33 -0300

    Adicionar botao Authorize no Swagger

--- Api.Application/Startup.cs
+++ Api.Application/Startup.cs
@@ -82,6 +82,25 @@ namespace application
                         Name = "Termo de Licença de Uso"
                     }
                 });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Entre com o token JWT",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement{
+                    {
+                        new OpenApiSecurityScheme{
+                            Reference=new OpenApiReference{
+                                Id = "Bearer",
+                                Type=ReferenceType.SecurityScheme
+                            }
+                        }, new List<string>()
+                    }
+                });
             });
         }
 
