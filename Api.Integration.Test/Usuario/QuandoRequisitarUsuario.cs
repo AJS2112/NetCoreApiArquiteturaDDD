21b64dbbaa98d69dab1d3986257fbdda67323ef3
Antonio Sucre <antonio.sucre@indikatore.com>
2021-12-25 21:42:14 -0300

    Implementar Metodo PUT

--- Api.Integration.Test/Usuario/QuandoRequisitarUsuario.cs
+++ Api.Integration.Test/Usuario/QuandoRequisitarUsuario.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Api.Domain.DTOs.User;
 using Newtonsoft.Json;
@@ -47,6 +49,22 @@ namespace Api.Integration.Test.Usuario
             Assert.NotNull(listaFromJson);
             Assert.True(listaFromJson.Count() > 0);
             Assert.True(listaFromJson.Where(r => r.Id == registroPost.Id).Count() == 1);
+
+            //PUT
+            var updateUserDto = new UserUpdateDTO()
+            {
+                Id = registroPost.Id,
+                Name = Faker.Name.First(),
+                Email = Faker.Internet.Email()
+            };
+            var stringContent = new StringContent(JsonConvert.SerializeObject(updateUserDto), Encoding.UTF8, "application/json");
+
+            response = await client.PutAsync($"{hostApi}users", stringContent);
+            jsonResult = await response.Content.ReadAsStringAsync();
+            var registroAtualizado = JsonConvert.DeserializeObject<UserUpdateResultDTO>(jsonResult);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotEqual(registroPost.Name, registroAtualizado.Name);
         }
 
     }
